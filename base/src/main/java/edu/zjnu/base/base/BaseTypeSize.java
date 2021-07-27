aefe55dfe45e93138fc97deac05bb2eed603b354
杨海波 <3546514206@qq.com>
2021-07-27 19:59:49 +0800

    基本类型测试

---
+++ base/src/main/java/edu/zjnu/base/base/BaseTypeSize.java
@@ -0,0 +1,19 @@
+package edu.zjnu.base.base;
+
+/**
+ * @description: 基本类型的长度
+ * @author: 杨海波
+ * @date: 2021-07-26
+ **/
+public class BaseTypeSize {
+
+    public static void main(String[] args) {
+        System.out.println(Character.SIZE / 8);
+        System.out.println(Byte.SIZE / 8);
+        System.out.println(Short.SIZE / 8);
+        System.out.println(Integer.SIZE / 8);
+        System.out.println(Long.SIZE / 8);
+        System.out.println(Float.SIZE / 8);
+        System.out.println(Double.SIZE / 8);
+    }
+}
