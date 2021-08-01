9957e5832874c2d272e7e47e431f961e2d49014a
杨海波 <3546514206@qq.com>
2021-08-01 22:03:34 +0800

    增加spring boot模块

---
+++ boot/src/main/java/edu/zjnu/boot/BootApplication.java
@@ -0,0 +1,13 @@
+package edu.zjnu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+}
---
+++ boot/src/test/java/edu/zjnu/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zjnu.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
