43a7f6c91d62452f4c4c48fdbefda13461b82896
杨海波 <3546514206@qq.com>
2021-08-03 22:34:54 +0800

    SpringBoot 编程设置应用配置

--- boot/src/main/java/edu/zjnu/boot/BootApplication.java
+++ boot/src/main/java/edu/zjnu/boot/BootApplication.java
@@ -1,5 +1,6 @@
 package edu.zjnu.boot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+        SpringApplication application = new SpringApplication(BootApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
     }
 
 }
