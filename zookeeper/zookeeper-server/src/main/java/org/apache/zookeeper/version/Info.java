4eb4d46a0800377ba04c3889129e0a8e2522c4ef
杨海波 <3546514206@qq.com>
2021-08-05 20:17:58 +0800

    生成缺少代码 修改配置

---
+++ zookeeper/zookeeper-server/src/main/java/org/apache/zookeeper/version/Info.java
@@ -0,0 +1,16 @@
+package org.apache.zookeeper.version;
+
+/**
+ * @description: Info
+ * @author: 杨海波
+ * @date: 2021-08-05
+ **/
+public interface Info {
+    int MAJOR = 1;
+    int MINOR = 0;
+    int MICRO = 0;
+    String QUALIFIER = null;
+    int REVISION = -1;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2021-08-04";
+}
