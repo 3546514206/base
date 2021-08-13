0ed13c67153a17bf9d5ad3e726d3acfd5829628a
杨海波 <3546514206@qq.com>
2021-08-13 19:58:15 +0800

    接口之间可以多继承，类可以多实现接口

---
+++ base/src/main/java/edu/zjnu/base/oop/ABC.java
@@ -0,0 +1,23 @@
+package edu.zjnu.base.oop;
+
+/**
+ * @description: ABC
+ * @author: 杨海波
+ * @date: 2021-08-13
+ **/
+public class ABC implements InterFaceA {
+    @Override
+    public void a() {
+
+    }
+
+    @Override
+    public void b() {
+
+    }
+
+    @Override
+    public void c() {
+
+    }
+}
---
+++ base/src/main/java/edu/zjnu/base/oop/BC.java
@@ -0,0 +1,18 @@
+package edu.zjnu.base.oop;
+
+/**
+ * @description: BC
+ * @author: 杨海波
+ * @date: 2021-08-13
+ **/
+public class BC implements InterFaceB, InterFaceC {
+    @Override
+    public void b() {
+
+    }
+
+    @Override
+    public void c() {
+
+    }
+}
