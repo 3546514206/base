3e5333c8aaa6fdb9162598bfbc696808afb1b63d
杨海波 <3546514206@qq.com>
2021-09-02 09:34:57 +0800

    qualifier

---
+++ src/main/java/edu/zjnu/spring/ioc/qualifier/QualifierBean.java
@@ -0,0 +1,13 @@
+package edu.zjnu.spring.ioc.qualifier;
+
+/**
+ * @description: QualifierBean
+ * @author: 杨海波
+ * @date: 2021-09-02
+ **/
+public class QualifierBean {
+
+    public void func(){
+        System.out.println(" do something");
+    }
+}
