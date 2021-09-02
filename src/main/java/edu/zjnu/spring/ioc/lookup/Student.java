af1e03ffbff48a3226cd3eed35918958b4421ef3
杨海波 <3546514206@qq.com>
2021-09-02 08:56:09 +0800

    look up 作用场景

---
+++ src/main/java/edu/zjnu/spring/ioc/lookup/Student.java
@@ -0,0 +1,13 @@
+package edu.zjnu.spring.ioc.lookup;
+
+/**
+ * @description: Student
+ * @author: 杨海波
+ * @date: 2021-09-02
+ **/
+public class Student extends User {
+
+    public void showMe(){
+        System.out.println("i am a student");
+    }
+}
