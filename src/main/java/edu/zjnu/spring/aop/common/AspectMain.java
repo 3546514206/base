2c7ea95d93a68eb46903869d1741b84846e63821
杨海波 <3546514206@qq.com>
2021-09-04 22:16:25 +0800

    Aspect的使用

---
+++ src/main/java/edu/zjnu/spring/aop/common/AspectMain.java
@@ -0,0 +1,18 @@
+package edu.zjnu.spring.aop.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @description: AspectMain
+ * @author: 杨海波
+ * @date: 2021-09-04
+ **/
+public class AspectMain {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-aspect.xml");
+        TestBean bean =(TestBean)  context.getBean("testBean");
+        bean.test();
+    }
+}
