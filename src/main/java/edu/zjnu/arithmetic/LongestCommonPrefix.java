2cb21657536fcc132f17f78cdc8a2930d4a3fea0
杨海波 <3546514206@qq.com>
2021-09-09 21:28:38 +0800

    横向扩展法

--- src/main/java/edu/zjnu/arithmetic/LongestCommonPrefix.java
+++ src/main/java/edu/zjnu/arithmetic/LongestCommonPrefix.java
@@ -13,20 +13,38 @@ public class LongestCommonPrefix {
         String stringC = "asdwran";
         String[] strs = new String[]{stringA, stringB, stringC};
 
-        String rs = new LongestCommonPrefix().longestCommonPrefix(strs);
+        String rs = new LongestCommonPrefix().sulotion(strs);
+        String rs1 = new LongestCommonPrefix().longestCommonPrefix(strs);
 
-        System.out.println(rs);
+        System.out.println(rs + " : " + rs1);
 
     }
 
     /**
-     * 
+     * 横向扩展法
+     *
      * @param strs
      * @return
      */
     public String sulotion(String[] strs) {
+        if (0 == strs.length) {
+            return "";
+        }
 
-        return null;
+        String rs = strs[0];
+
+        for (int i = 1; i <= strs.length - 1; i++) {
+            for (int j = 0; j < rs.length() && j < strs[i].length(); j++) {
+                if (rs.charAt(j) != strs[i].charAt(j)) {
+                    rs = rs.substring(0, j);
+                }
+            }
+            if ("".equals(rs)) {
+                break;
+            }
+        }
+
+        return rs;
     }
 
 
