6767191fbf8718bc628d8ff280c1ef2123d59709
杨海波 <3546514206@qq.com>
2021-09-14 19:54:13 +0800

    树的节点

---
+++ src/main/java/edu/zjnu/datastructure/tree/TreeNode.java
@@ -0,0 +1,16 @@
+package edu.zjnu.datastructure.tree;
+
+/**
+ * @description: TreeNode
+ * @author: 杨海波
+ * @date: 2021-09-14
+ **/
+public class TreeNode<T> {
+
+    public T value;
+
+    public TreeNode<T> left;
+
+    public TreeNode<T> right;
+    
+}
