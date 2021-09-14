53aa43589721860aaa1e41cbcd3abb95be1e8bc1
杨海波 <3546514206@qq.com>
2021-09-15 00:42:10 +0800

    堆区测试

---
+++ src/main/java/edu/zjnu/base/jvm/HeapMain.java
@@ -0,0 +1,32 @@
+package edu.zjnu.base.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description: 堆区测试
+ * @author: 杨海波
+ * @date: 2021-09-14
+ **/
+public class HeapMain {
+
+    public static void main(String[] args) {
+        List<HeapObject> objects = new ArrayList<>();
+        // 所有的 HeapObject 都被 objects 引用，不会被回收
+        while (true) {
+            //1KB
+            objects.add(new HeapObject(new byte[1024]));
+        }
+    }
+
+
+}
+
+class HeapObject {
+
+    private byte[] a;
+
+    public HeapObject(byte[] a) {
+        this.a = a;
+    }
+}
