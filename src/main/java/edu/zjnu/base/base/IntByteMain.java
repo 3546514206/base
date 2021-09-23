8eaade883c855474bf8a3b1a5ccf2c786bb2d47f
杨海波 <3546514206@qq.com>
2021-09-23 23:06:16 +0800

    一道笔试题

---
+++ src/main/java/edu/zjnu/base/base/IntByteMain.java
@@ -0,0 +1,14 @@
+package edu.zjnu.base.base;
+
+/**
+ * @description: todo
+ * @author: 杨海波
+ * @date: 2021-09-23
+ **/
+public class IntByteMain {
+    public static void main(String[] args) {
+        int i = 0x1111;
+        System.out.println((byte) i);
+
+    }
+}
