697c47e24d3899e293534d452d652503b7fa3a83
杨海波 <3546514206@qq.com>
2021-09-26 22:53:13 +0800

    位运算

---
+++ src/main/java/edu/zjnu/base/base/XOR.java
@@ -0,0 +1,18 @@
+package edu.zjnu.base.base;
+
+/**
+ * @description: todo
+ * @author: 杨海波
+ * @date: 2021-09-26
+ **/
+public class XOR {
+
+    public static void main(String[] args) {
+        System.out.println(0b0100 | 0b0010);
+        System.out.println(0b0111 ^ 0b0011);
+        System.out.println(7 ^ 2);
+        System.out.println(0b0110 & 0b0010);
+        System.out.println(0b0110 << 1);
+        System.out.println(0b0110 << 3);
+    }
+}
