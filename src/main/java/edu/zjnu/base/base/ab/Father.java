49f9466518e5a8529e5879f1c84bf068c8dfd0fc
杨海波 <3546514206@qq.com>
2021-11-11 13:36:36 +0800

    抽象类

---
+++ src/main/java/edu/zjnu/base/base/ab/Father.java
@@ -0,0 +1,13 @@
+package edu.zjnu.base.base.ab;
+
+/**
+ * @description: todo
+ * @author: 杨海波
+ * @date: 2021-11-11
+ **/
+public abstract class Father {
+
+    public Father() {
+        System.out.println("抽象类的无参数方法");
+    }
+}
---
+++ src/main/java/edu/zjnu/base/base/ab/Son.java
@@ -0,0 +1,13 @@
+package edu.zjnu.base.base.ab;
+
+/**
+ * @description: todo
+ * @author: 杨海波
+ * @date: 2021-11-11
+ **/
+public class Son extends Father {
+
+    public Son() {
+        System.out.println("实现类的无参数构造方法");
+    }
+}
