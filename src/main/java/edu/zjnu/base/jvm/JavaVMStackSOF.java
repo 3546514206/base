53b8dd84dfad625226e54831739ce904aa68f81d
杨海波 <3546514206@qq.com>
2021-11-30 19:11:00 +0800

    内存溢出

---
+++ src/main/java/edu/zjnu/base/jvm/JavaVMStackSOF.java
@@ -0,0 +1,29 @@
+package edu.zjnu.base.jvm;
+
+/**
+ * @description: todo
+ * @author: 杨海波
+ * @date: 2021-11-30
+ **/
+public class JavaVMStackSOF {
+
+    private int stackLength = -1;
+
+    public static void main(String[] args) {
+        JavaVMStackSOF javaVMStackSOF = new JavaVMStackSOF();
+        try {
+            javaVMStackSOF.stackLeak();
+        } catch (Throwable e) {
+            System.out.println("stack length: " + javaVMStackSOF.stackLength);
+            throw e;
+        }
+
+
+    }
+
+    public void stackLeak() {
+        stackLength++;
+        stackLeak();
+    }
+
+}
