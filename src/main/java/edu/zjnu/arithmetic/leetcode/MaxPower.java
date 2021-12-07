3541cdbe2a01eaa831fcf5b88d9ab004d5a538bf
杨海波 <3546514206@qq.com>
2021-12-07 16:06:30 +0800

    每日一题，一题多解

--- src/main/java/edu/zjnu/arithmetic/leetcode/MaxPower.java
+++ src/main/java/edu/zjnu/arithmetic/leetcode/MaxPower.java
@@ -8,8 +8,35 @@ package edu.zjnu.arithmetic.leetcode;
 public class MaxPower {
 
     public static void main(String[] args) {
-        System.out.println(new MaxPower().maxPowerV1("leetcode"));
+        System.out.println(new MaxPower().maxPower("leetcode"));
     }
+
+
+    public int maxPower(String s) {
+
+        s = s + "#";
+        int res = 0;
+        int index = 0;
+        int count = 0;
+
+        int len = s.length();
+
+        for (int i = 0; i < len; i++) {
+
+            if (s.charAt(i) == s.charAt(index)) {
+                count++;
+            } else {
+                index = i;
+                res = Math.max(res, count);
+                count = 1;
+            }
+
+
+        }
+
+        return res;
+    }
+
 
     public int maxPowerV1(String s) {
 
