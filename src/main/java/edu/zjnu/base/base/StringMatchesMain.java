1468db54a00b336f152339ebf4fdb9ba46dd9e20
杨海波 <3546514206@qq.com>
2022-03-23 08:16:28 +0800

    matches

---
+++ src/main/java/edu/zjnu/base/base/StringMatchesMain.java
@@ -0,0 +1,9 @@
+package edu.zjnu.base.base;
+
+public class StringMatchesMain {
+
+    public static void main(String[] args) {
+        String s = "easy";
+        System.out.println(s.matches("*"));
+    }
+}
