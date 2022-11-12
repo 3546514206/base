140e47b38e65f511a6137d11ca5e8905e6b5bc3c
杨海波 <3546514206@qq.com>
2022-11-12 13:43:43 +0800

    提交

---
+++ src/main/java/edu/zjnu/arithmetic/zcy/BinSearchMain.java
@@ -0,0 +1,14 @@
+package edu.zjnu.arithmetic.zcy;
+
+/**
+ * @author: 杨海波
+ * @date: 2022-10-26 19:11:55
+ * @description: BinSearchMain
+ */
+public class BinSearchMain {
+
+    public static void main(String[] args) {
+        int [] arr1 = new int[]{1,2};
+    }
+
+}
