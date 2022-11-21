46ef9eab8a9a85a48af6f00ffb8a39b60d39df95
杨海波 <3546514206@qq.com>
2022-11-21 23:22:32 +0800

    c++ 的三种继承方式

--- ccpp/access_modifiers_protected/main.cpp
+++ ccpp/access_modifiers_protected/main.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<assert.h>
 
 using namespace std;
 
@@ -28,10 +27,8 @@ protected:
 private:
     int a3;
 };
-
-// 即便不写 private ，默认的继承方式也是 private
-// private 继承：基类 public 成员，protected 成员的访问属性在派生类中分别变成：private, private; 基类的 private 派生类无法访问
-class B : private A {
+// protected 继承：基类 public 成员，protected 成员的访问属性在派生类中分别变成：protected, protected, 基类的 private 属性子类无法继承。
+class B : protected A {
 public:
     int a;
 
@@ -42,8 +39,8 @@ public:
 
     void fun() {
         cout << a << endl;       //正确，public成员。
-        cout << a1 << endl;       //正确，基类public成员,在派生类中变成了private,可以被派生类访问。
-        cout << a2 << endl;       //正确，基类的protected成员，在派生类中变成了private,可以被派生类访问。
+        cout << a1 << endl;       //正确，基类的public成员，在派生类中变成了protected，可以被派生类访问。
+        cout << a2 << endl;       //正确，基类的protected成员，在派生类中还是protected，可以被派生类访问。
 //        cout << a3 << endl;       //错误，基类的private成员不能被派生类访问。
     }
 };
@@ -51,8 +48,8 @@ public:
 int main() {
     B b(10);
     cout << b.a << endl;       //正确。public成员
-//    cout << b.a1 << endl;      //错误，private成员不能在类外访问。
-//    cout << b.a2 << endl;      //错误, private成员不能在类外访问。
+//    cout << b.a1 << endl;      //错误，protected成员不能在类外访问。Java 中是可以用在类外访问protected成员的
+//    cout << b.a2 << endl;      //错误，protected成员不能在类外访问。
 //    cout << b.a3 << endl;      //错误，private成员不能在类外访问。
     system("pause");
     return 0;
