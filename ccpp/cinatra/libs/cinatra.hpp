537a1b9e9621b36f71cabba5b3876136bd04b65a
杨海波 <3546514206@qq.com>
2022-11-21 17:35:42 +0800

    web 服务

---
+++ ccpp/cinatra/libs/cinatra.hpp
@@ -0,0 +1,12 @@
+//
+// Created by qicosmos on 2/13/19.
+//
+
+#ifndef CINATRA_CINATRA_HPP
+#define CINATRA_CINATRA_HPP
+
+#include "cinatra/client_factory.hpp"
+#include "cinatra/smtp_client.hpp"
+#include "cinatra/http_server.hpp"
+
+#endif // CINATRA_CINATRA_HPP
