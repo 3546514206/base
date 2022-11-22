ac3eeccee7d6a8ebe10f6501decdc0611685ca42
杨海波 <3546514206@qq.com>
2022-11-22 16:14:34 +0800

    一元运算符实例

--- ccpp/operator_freight/main.cpp
+++ ccpp/operator_freight/main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 
-//您可以重定义或重载大部分 C++ 内置的运算符。这样，您就能使用自定义类型的运算符。
-//重载的运算符是带有特殊名称的函数，函数名是由关键字 operator 和其后要重载的运算
-//符符号构成的。与其他函数一样，重载运算符有一个返回类型和一个参数列表。例如：
-//      Box operator+(const Box&);
-
 
 class box {
 public:
@@ -40,6 +35,11 @@ void box::set_height(double height) {
     this->height = height;
 }
 
+//您可以重定义或重载大部分 C++ 内置的运算符。这样，您就能使用自定义类型的运算符。
+//重载的运算符是带有特殊名称的函数，函数名是由关键字 operator 和其后要重载的运算
+//符符号构成的。与其他函数一样，重载运算符有一个返回类型和一个参数列表。例如：
+//      Box operator+(const Box&);
+//运算符重载实际上改变了运算符的操作元素
 box box::operator+(const box &b) {
     box *new_box = new box;
     new_box->width = this->width + b.width;
@@ -64,9 +64,9 @@ int main() {
 
     box3 = box1 + box2;
 
-    std::cout<<"box1 的体积："<< box1.get_volume()<<std::endl;
-    std::cout<<"box2 的体积："<< box2.get_volume()<<std::endl;
-    std::cout<<"box3 的体积："<< box3.get_volume()<<std::endl;
+    std::cout << "box1 的体积：" << box1.get_volume() << std::endl;
+    std::cout << "box2 的体积：" << box2.get_volume() << std::endl;
+    std::cout << "box3 的体积：" << box3.get_volume() << std::endl;
 
     return 0;
 }
