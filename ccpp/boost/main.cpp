53ed6ac28ed3a5c2251219b7e2f19c05313a33e0
杨海波 <3546514206@qq.com>
2022-11-25 21:23:03 +0800

    线程使用

---
+++ ccpp/boost/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "thread.hpp"
+
+
+
+int main() {
+
+    thread_testing_space::test_thread();
+
+    return 0;
+}
+
---
+++ ccpp/boost/thread.hpp
@@ -0,0 +1,17 @@
+//
+// Created by 杨海波 on 2022/11/25.
+//
+
+#ifndef BOOST_THREAD_HPP
+#define BOOST_THREAD_HPP
+
+#include <boost/version.hpp>
+#include <boost/config.hpp>
+#include <boost/thread.hpp>
+#include <iostream>
+
+namespace thread_testing_space{
+    void test_thread();
+}
+
+#endif //BOOST_THREAD_HPP
