7a354f99d2e2b80c02976bbadf1583b27ed0742e
杨海波 <3546514206@qq.com>
2022-11-25 22:10:45 +0800

    手动抛出信号

---
+++ ccpp/signal_raise/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+using namespace std;
+
+void signalHandler(int signum) {
+    cout << "Interrupt signal (" << signum << ") received.\n";
+
+    // 清理并关闭
+    // 终止程序
+
+    exit(signum);
+
+}
+
+int main() {
+    int i = 0;
+    // 注册信号 SIGINT 和信号处理程序
+    signal(SIGINT, signalHandler);
+
+    while (++i) {
+        cout << "Going to sleep...." << endl;
+        if (i == 3) {
+            raise(SIGINT);
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
