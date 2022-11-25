c33c171aa9fbb8f7c71e6760b6cd7edb06cf69b8
杨海波 <3546514206@qq.com>
2022-11-25 21:58:16 +0800

    信号处理

--- ccpp/signal/main.cpp
+++ ccpp/signal/main.cpp
@@ -2,31 +2,26 @@
 #include <csignal>
 #include <unistd.h>
 
-// 信号处理测试
-void signal_handler(int sig_num) {
-    std::cout << "Interrupt signal (" << sig_num << ") received.\n";
+using namespace std;
+
+void signalHandler(int signum) {
+    cout << "Interrupt signal (" << signum << ") received.\n";
 
     // 清理并关闭
     // 终止程序
 
-    exit(sig_num);
+    exit(signum);
+
 }
 
 int main() {
-    // 向操作系统注册信号处理函数
-    signal(SIGFPE, signal_handler);
-    int i = 0;
+    // 注册信号 SIGINT 和信号处理程序
+    signal(SIGINT, signalHandler);
 
-    while (true) {
-        i++;
-        std::cout << "Going to sleep...." << std::endl;
-        if (i == 5) {
-            int j = i / 0;
-            std::cout << "division by zero" << std::endl;
-        }
+    while (1) {
+        cout << "Going to sleep...." << endl;
         sleep(1);
     }
 
-    return 0;
     return 0;
 }
