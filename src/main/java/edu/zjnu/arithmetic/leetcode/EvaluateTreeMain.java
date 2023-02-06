b6f6a9cd42005cc910b627b69c43b6b7ef8acb03
杨海波 <3546514206@qq.com>
2023-02-06 14:15:50 +0800

    每日一题

---
+++ src/main/java/edu/zjnu/arithmetic/leetcode/EvaluateTreeMain.java
@@ -0,0 +1,39 @@
+package edu.zjnu.arithmetic.leetcode;
+
+/**
+ * @author: 杨海波
+ * @date: 2023-02-06 09:14:40
+ * @description: EvaluateTreeMain
+ */
+public class EvaluateTreeMain {
+
+    public boolean evaluateTree(TreeNode root) {
+
+
+        if (root == null) {
+            return false;
+        }
+
+        return compute(root);
+    }
+
+    private boolean compute(TreeNode node) {
+        if (node.left != null && node.right != null) {
+            boolean leftValue = compute(node.left);
+            boolean rightValue = compute(node.right);
+
+            if (node.val == 2) {
+                return leftValue || rightValue;
+            }
+
+            if (node.val == 3) {
+                return leftValue && rightValue;
+            }
+
+            return false;
+        }
+
+        return node.val == 1;
+    }
+
+}
