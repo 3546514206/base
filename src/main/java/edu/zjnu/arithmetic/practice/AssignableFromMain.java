70b2dc2a846dfd61d4198181739781333a886d36
杨海波 <3546514206@QQ.COM>
2023-04-29 02:26:46 +0800

    cmake

--- src/main/java/edu/zjnu/arithmetic/practice/AssignableFromMain.java
+++ src/main/java/edu/zjnu/arithmetic/practice/AssignableFromMain.java
@@ -1,6 +1,20 @@
-package edu.zjnu.arithmetic.practice;/**
+package edu.zjnu.arithmetic.practice;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
  * @author: 杨海波
  * @date: 2023-04-27 13:00:09
  * @description: todo
- */public class AssignableFromMain {
+ */
+public class AssignableFromMain {
+
+    public static void main(String[] args) {
+        Map<String,Object> v1 = new HashMap<>();
+        String v2 = "";
+        System.out.println(v1.getClass().isAssignableFrom(v2.getClass()));
+        System.out.println(v1.getClass().isAssignableFrom(v2.getClass()) || v2.getClass().isAssignableFrom(v1.getClass()));
+    }
 }
