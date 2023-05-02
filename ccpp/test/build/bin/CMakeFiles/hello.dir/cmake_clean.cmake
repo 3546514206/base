1e54861c6b8c19525808b642ea3860457ad981ef
杨海波 <3546514206@qq.com>
2023-05-03 02:16:47 +0800

    test

---
+++ ccpp/test/build/bin/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/main.o"
+  "CMakeFiles/hello.dir/main.o.d"
+  "hello"
+  "hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
