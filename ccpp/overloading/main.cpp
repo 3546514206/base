166f0c49a6e47641e14c863f6f0a63cac510ca4c
杨海波 <3546514206@qq.com>
2023-05-14 19:46:45 +0800

    存在歧义的构造函数

---
+++ ccpp/overloading/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class complex {
+public:
+    complex(double x = 1, double y = 2) : re(x), im(y) {
+
+    }
+
+    complex() : re(0), im(0) {
+
+    }
+
+private:
+    double
+//    实部
+    re,
+//    虚部
+    im;
+};
+
+int main() {
+//    构造函数存在歧义
+//    complex c1;
+// 构造函数不存在歧义
+    complex c2();
+    return 0;
+}
