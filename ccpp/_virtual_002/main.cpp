0a3474d4952d87d967fce46fb69800d74241c75d
杨海波 <3546514206@qq.com>
2023-05-26 08:53:27 +0800

    虚函数动态绑定

---
+++ ccpp/_virtual_002/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Base {
+public:
+    virtual void foo() { std::cout << "Base::foo()" << std::endl; }
+};
+
+class Derived : public Base {
+public:
+    void foo() override { std::cout << "Derived::foo()" << std::endl; }
+};
+
+int main() {
+    Derived obj;
+    Base *ptr = static_cast<Base *>(&obj);
+    ptr->foo();
+    return 0;
+}
+
