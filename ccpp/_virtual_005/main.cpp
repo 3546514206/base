11e424d4d753ef1708ae517bc53300b1904e1e85
杨海波 <3546514206@qq.com>
2023-06-05 16:32:15 +0800

    指针大小和虚函数表的存在

---
+++ ccpp/_virtual_005/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class A {
+public:
+    int x;
+
+    virtual void f() {}
+};
+
+
+int main() {
+
+    auto *p = new A;
+    std::cout << p << std::endl;
+    std::cout << sizeof(p) << std::endl;
+    std::cout << &(p->x) << std::endl;
+
+    return 0;
+}
