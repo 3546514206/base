b0ddafdc814f801026774ec05cf06460729be722
杨海波 <3546514206@qq.com>
2023-06-20 09:43:32 +0800

    职责链模式

---
+++ ccpp/_design_chain/main.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <utility>
+
+class Handler {
+private:
+    std::string name;
+protected:
+
+    Handler *successor;
+public:
+
+    virtual void setSuccessor(Handler *successor) {
+        this->successor = successor;
+    }
+
+    std::string getName() {
+        return this->name;
+    }
+
+    void setName(const std::string &name) {
+        this->name = name;
+    }
+
+    virtual void handleRequest(const std::string &request) = 0;
+};
+
+class ConcreteHandlerA : public Handler {
+public:
+
+    void handleRequest(const std::string &request) override {
+        if (request == "A") {
+            std::cout << "ConcreteHandlerA handles the request." << std::endl;
+        }
+
+        if (this->successor != nullptr) {
+            successor->handleRequest(request);
+        } else {
+            std::cout << "No handler can process the request after " << getName() << std::endl;
+        }
+    }
+};
+
+class ConcreteHandlerB : public Handler {
+public:
+
+    void handleRequest(const std::string &request) override {
+        if (request == "B") {
+            std::cout << "ConcreteHandlerB handles the request. " << std::endl;
+        }
+        if (successor != nullptr) {
+            successor->handleRequest(request);
+        } else {
+            std::cout << "No handler can process the request after " << getName() << std::endl;
+        }
+    }
+};
+
+class ConcreteHandlerC : public Handler {
+public:
+    void handleRequest(const std::string &request) override {
+        if (request == "C") {
+            std::cout << "ConcreteHandlerC handles the request." << std::endl;
+        }
+        if (successor != nullptr) {
+            successor->handleRequest(request);
+        } else {
+            std::cout << "No handler can process the request after " << getName() << std::endl;
+        }
+    }
+};
+
+int main() {
+    Handler *handlerA = new ConcreteHandlerA();
+    handlerA->setName("ConcreteHandlerA");
+    Handler *handlerB = new ConcreteHandlerB();
+    handlerB->setName("ConcreteHandlerB");
+    Handler *handlerC = new ConcreteHandlerC();
+    handlerC->setName("ConcreteHandlerC");
+
+    handlerA->setSuccessor(handlerB);
+    handlerB->setSuccessor(handlerC);
+
+    handlerA->handleRequest("A");
+
+    return 0;
+}
