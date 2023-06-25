9303534361b12c99a3bbe3d721f2ea3df2539253
杨海波 <3546514206@QQ.COM>
2023-06-25 15:03:46 +0800

    日常学习提交

---
+++ src/main/java/edu/zjnu/base/net/RabbitMQ/Consumer.java
@@ -0,0 +1,6 @@
+package edu.zjnu.base.net.RabbitMQ;/**
+ * @author: 杨海波
+ * @date: 2023-06-25 08:48:54
+ * @description: todo
+ */public class Consumer {
+}
---
+++ src/main/java/edu/zjnu/base/net/RabbitMQ/Producer.java
@@ -0,0 +1,6 @@
+package edu.zjnu.base.net.RabbitMQ;/**
+ * @author: 杨海波
+ * @date: 2023-06-25 08:48:25
+ * @description: todo
+ */public class Producer {
+}
