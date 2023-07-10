9fc4db4e4172aabf3da9e289b9c5422d8e9dba47
杨海波 <3546514206@QQ.COM>
2023-07-10 09:24:07 +0800

    力扣第20题

---
+++ ccpp/_leetcode_0020/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s);
+};
+
+bool Solution::isValid(string s)
+{
+    int n = s.size();
+    if (n % 2 == 1)
+    {
+        return false;
+    }
+
+    unordered_map<char, char> pairs = {
+        {')', '('},
+        {']', '['},
+        {'}', '{'}};
+
+    stack<char> stak;
+    for (char ch : s)
+    {
+        if (pairs.count(ch))
+        {
+            if (stak.empty() || stak.top() != pairs[ch])
+            {
+                return false;
+            }
+
+            stak.pop();
+        }
+        else
+        {
+            stak.push(ch);
+        }
+    }
+
+    return stak.empty();
+}
+
+int main()
+{
+    return 0;
+}
