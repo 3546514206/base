4e0bade2294841ff635d30db64304c7b00685f34
杨海波 <99322388+3546514206@users.noreply.github.com>
2023-10-31 22:04:15 +0800

    更新

--- CCPP/thread_002/main.cpp
+++ CCPP/thread_002/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define NUM_THREADS 5
 
 // 线程的运行函数
-void* say_hello(void* args)
+void *say_hello(void *args)
 {
     cout << "Hello w3cschool!" << endl;
 }
@@ -16,15 +16,15 @@ int main()
 {
     // 定义线程的 id 变量，多个变量使用数组
     pthread_t tids[NUM_THREADS];
-    for(int i = 0; i < NUM_THREADS; ++i)
+    for (int i = 0; i < NUM_THREADS; ++i)
     {
-        //参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
+        // 参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
         int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
         if (ret != 0)
         {
-           cout << "pthread_create error: error_code=" << ret << endl;
+            cout << "pthread_create error: error_code=" << ret << endl;
         }
     }
-    //等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
+    // 等各个线程退出后，进程才结束，否则进程强制结束了，线程可能还没反应过来；
     pthread_exit(NULL);
 }
