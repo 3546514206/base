1f96e4d87efb78114f180e04e72fb926618607f4
杨海波 <3546514206@qq.com>
2023-11-03 00:52:01 +0800

    提交

--- CCPP/thread_001/main.cpp
+++ CCPP/thread_001/main.cpp
@@ -7,21 +7,17 @@ using namespace std;
 #define NUM_THREADS 5
 
 // 线程的运行函数
-void *say_hello(void *args)
-{
+void *say_hello(void *args) {
     cout << "Hello w3cschool！" << endl;
 }
 
-int main()
-{
+int main() {
     // 定义线程的 id 变量，多个变量使用数组
     pthread_t tids[NUM_THREADS];
-    for (int i = 0; i < NUM_THREADS; ++i)
-    {
+    for (int i = 0; i < NUM_THREADS; ++i) {
         // 参数依次是：创建的线程id，线程参数，调用的函数，传入的函数参数
         int ret = pthread_create(&tids[i], NULL, say_hello, NULL);
-        if (ret != 0)
-        {
+        if (ret != 0) {
             cout << "pthread_create error: error_code=" << ret << endl;
         }
     }
