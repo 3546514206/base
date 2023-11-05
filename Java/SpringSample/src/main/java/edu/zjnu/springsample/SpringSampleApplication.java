3d4fe3f598f86b2e8b8d75755c1b889e962acdcf
杨海波 <3546514206@qq.com>
2023-11-05 20:41:55 +0800

    更新

---
+++ Java/SpringSample/src/main/java/edu/zjnu/springsample/SpringSampleApplication.java
@@ -0,0 +1,13 @@
+package edu.zjnu.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSampleApplication.class, args);
+    }
+
+}
---
+++ Java/SpringSample/src/test/java/edu/zjnu/springsample/SpringSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.zjnu.springsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
