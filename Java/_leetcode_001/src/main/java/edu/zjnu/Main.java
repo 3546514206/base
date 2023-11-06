42690038bc189c598dbe258e1c4205b219801a3d
杨海波 <3546514206@qq.com>
2023-11-06 11:02:44 +0800

    更新

---
+++ Java/_leetcode_001/src/main/java/edu/zjnu/Main.java
@@ -0,0 +1,13 @@
+package edu.zjnu;
+
+/**
+ * @author: 杨海波
+ * @date: 2023-11-06 09:08:38
+ * @description: 有人相爱，有人夜里开车看海，有人力扣第一道题都做不出来。
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
