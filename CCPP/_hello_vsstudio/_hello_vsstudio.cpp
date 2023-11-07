17578f59265d75b9157a053b1e023b64b3362c35
杨海波 <3546514206@qq.com>
2023-11-07 21:36:26 +0800

    更新

---
+++ CCPP/_hello_vsstudio/_hello_vsstudio.cpp
@@ -0,0 +1,12 @@
+﻿// _hello_vsstudio.cpp: 定义应用程序的入口点。
+//
+
+#include "_hello_vsstudio.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ CCPP/_hello_vsstudio_001/_hello_vsstudio_001.cpp
@@ -0,0 +1,12 @@
+﻿// _hello_vsstudio_001.cpp: 定义应用程序的入口点。
+//
+
+#include "_hello_vsstudio_001.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
