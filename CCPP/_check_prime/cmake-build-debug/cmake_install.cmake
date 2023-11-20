2975c2a7d9fa826bc737f1c4dbd511adcd266851
杨海波 <3546514206@qq.com>
2023-11-20 11:08:58 +0800

    更新

--- CCPP/_check_prime/cmake-build-debug/cmake_install.cmake
+++ CCPP/_check_prime/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_check_prime
+# Install script for directory: /Users/setsunayang/Documents/learning/base/CCPP/_check_prime
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/_check_prime")
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2021.3.4/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "/Library/Developer/CommandLineTools/usr/bin/objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_check_prime/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/setsunayang/Documents/learning/base/CCPP/_check_prime/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
