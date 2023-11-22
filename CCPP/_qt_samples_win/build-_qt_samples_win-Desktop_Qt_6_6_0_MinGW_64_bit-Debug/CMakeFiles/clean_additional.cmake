551cc66071b71abcc268a0ca21bf9cc4b23b6149
杨海波 <3546514206@qq.com>
2023-11-22 23:14:57 +0800

    更新

---
+++ CCPP/_qt_samples_win/build-_qt_samples_win-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\_qt_samples_win_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\_qt_samples_win_autogen.dir\\ParseCache.txt"
+  "_qt_samples_win_autogen"
+  )
+endif()
---
+++ CCPP/_qt_samples_win/build-_qt_samples_win-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -0,0 +1,10 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_qt_samples_win/build-_qt_samples_win-Desktop_Qt_6_6_0_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.6.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/6.6.0/mingw_64" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qt/Tools/mingw1120_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Qt/Tools/mingw1120_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
