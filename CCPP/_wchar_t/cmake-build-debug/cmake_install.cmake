378060ce92b3784224b24a091a71670fbb7d4f49
杨海波 <3546514206@qq.com>
2023-11-27 21:13:26 +0800

    宽字符

--- CCPP/_wchar_t/cmake-build-debug/cmake_install.cmake
+++ CCPP/_wchar_t/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_kruskal
+# Install script for directory: C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_wchar_t
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/_kruskal")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/_wchar_t")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_kruskal/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/SetsunaYang/Documents/GitHub/base/CCPP/_wchar_t/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
---
+++ CCPP/_wchar_t/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "杨海波" << std::endl;
+    std::cout << L'养' << std::endl;
+    return 0;
+}
