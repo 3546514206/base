4f997108a522c1a1cab765be4ea1c99553fbf900
杨海波 <3546514206@qq.com>
2023-12-09 21:38:17 +0800

    switch 语句

---
+++ JavaScript/_base/my_switch.js
@@ -0,0 +1,18 @@
+var fruit = "apple";
+
+switch (fruit) {
+    case "apple" :
+        console.log("11");
+        fruit = "banana";
+        break;
+    case "banana" :
+        console.log("22");
+
+    // fruit = "pear";
+    case "pear":
+        console.log("33");
+
+    default :
+        console.log("44");
+
+}
