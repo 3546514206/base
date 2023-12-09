8ddb7c0d5408063004190d533821d413bf2e9b0d
杨海波 <3546514206@qq.com>
2023-12-09 22:29:17 +0800

    不要问为什么，记住！避免bug

---
+++ JavaScript/_base/null_is_a_object.js
@@ -0,0 +1,6 @@
+let obj = {key: "value"};
+console.log(obj instanceof Object);  // 输出 true
+
+let n = null;
+console.log(n instanceof Object);   // 输出 false，因为 null 不是 Object 的实例
+console.log(typeof n === "object");
