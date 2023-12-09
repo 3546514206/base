35311979e15ecf19cd2e108ebf3a932619061f4e
杨海波 <3546514206@qq.com>
2023-12-09 22:59:04 +0800

    打好基本功，少写bug

--- JavaScript/_base/null_is_a_object.js
+++ JavaScript/_base/null_is_a_object.js
@@ -3,4 +3,4 @@ console.log(obj instanceof Object);  // 输出 true
 
 let n = null;
 console.log(n instanceof Object);   // 输出 false，因为 null 不是 Object 的实例
-console.log(typeof n === "object");
+console.log(typeof n === "object");  // 输出 true
