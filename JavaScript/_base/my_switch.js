d275a3ef5838fee801ddc48e056242b95711e17e
杨海波 <3546514206@qq.com>
2023-12-09 21:45:20 +0800

    switch 语句

--- JavaScript/_base/my_switch.js
+++ JavaScript/_base/my_switch.js
@@ -15,3 +15,15 @@ switch (fruit) {
         console.log("44");
 
 }
+
+var a = 3;
+
+// 需要注意的是，switch语句后面的表达式，与case语句后面的表
+// 示式比较运行结果时，采用的是严格相等运算符（===），而不是相等运算符（==），这意味着比较时不会发生类型转换。
+switch (a) {
+    case 1 + 2:
+        console.log(a);
+        break;
+    default:
+        console.log("default")
+}
