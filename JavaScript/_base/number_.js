63562214dc0c3907add9bf50392e295df2b72d8e
杨海波 <3546514206@qq.com>
2023-12-10 13:01:53 +0800

    NaN 相关

--- JavaScript/_base/number_.js
+++ JavaScript/_base/number_.js
@@ -21,3 +21,16 @@ console.log([2, 3, 4].indexOf("3"));
 console.log([4, 3, 2, 1][1]);
 // 数组内部索引的比较是不严格的
 console.log([4, 2]["0"]);
+
+// 以下是一些与数值相关的全局方法
+console.log(parseInt('123.8'));
+// 如果parseInt的参数不是字符串，则会先转为字符串再转换。
+// 对于那些会自动转为科学计数法的数字，parseInt会将科学计数法的表示方法视为字符串，因此导致一些奇怪的结果。
+parseInt(0.0000008); // 8
+// 等同于
+parseInt('8e-7'); // 8
+parseFloat(true);  // NaN
+
+console.log(isNaN(NaN));
+
+
