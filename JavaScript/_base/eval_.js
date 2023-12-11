5f38d062607f679924b22eecdb832c8a1d030098
杨海波 <3546514206@qq.com>
2023-12-11 16:34:35 +0800

    eval 关键字

--- JavaScript/_base/eval_.js
+++ JavaScript/_base/eval_.js
@@ -1,2 +1,15 @@
 eval('var a = 1;');
 console.log(a);
+
+
+// eval没有自己的作用域，都在当前作用域内执行
+// 但是，凡是使用别名执行eval，eval内部一律是全局作用域。
+var b = 1;
+
+function f() {
+    var b = 2;
+    var e = eval;
+    e('console.log(a)');
+}
+
+f() // 1
