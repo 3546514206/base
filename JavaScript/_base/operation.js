9385d17d625b4f5681cc404d07ea1e0566fa2986
杨海波 <3546514206@qq.com>
2023-12-12 09:23:17 +0800

    对象相加

--- JavaScript/_base/operation.js
+++ JavaScript/_base/operation.js
@@ -1,2 +1,16 @@
 console.log('3' + 4 + 5);
 console.log(3 + 4 + '5');
+var obj = {p: '1'};
+console.log(obj + 2);
+console.log(obj.toString());
+console.log(obj.valueOf());
+console.log(obj.valueOf().toString());
+
+var obj_1 = {
+    p: '1',
+    valueOf: function () {
+        return 1;
+    }
+};
+
+console.log(obj_1 + 2)
