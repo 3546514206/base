9c9a9c763d3fbdf78d49142a76b0543db39596bc
杨海波 <3546514206@qq.com>
2023-12-12 11:05:51 +0800

    error 构造函数

--- JavaScript/_base/error_.js
+++ JavaScript/_base/error_.js
@@ -1,2 +1,16 @@
 var err = new Error('出错了');
-console.log(err.message);
+console.log(err.message)
+
+function throwit() {
+    throw new Error('test');
+}
+
+function catchit() {
+    try {
+        throwit();
+    } catch (e) {
+        console.log(e.stack); // print stack trace
+    }
+}
+
+catchit();
