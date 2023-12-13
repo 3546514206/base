64f5012d2fa1ebbf8e847d289f0e023c075f3f10
杨海波 <3546514206@qq.com>
2023-12-13 23:01:46 +0800

    关键字 new 的内部逻辑

--- JavaScript/_base/mock_new.js
+++ JavaScript/_base/mock_new.js
@@ -1,9 +1,12 @@
 // 模拟 new 是如何执行一个构造函数的
-function _new(
-    /* 构造函数 */
-    constructor,
-    /* 构造函数参数 */
-    params) {
+// s1:创建一个新的空对象
+// s2:将这个空对象与当前执行上下文的this指针指向该空对象
+// s3:将该对象的原型属性（__proto__）指向构造函数的原型属性，构造新的原型链
+// s4:执行构造函数预定义的初始化逻辑
+// 如果当前构造函数没有显式返回一个对象，则将this返回，返回this是隐式的；如果当前对象显式返回了一个对象，则new得到的是这个对象，而不是this指向的对象。
+function _new(/* 构造函数 */
+              constructor, /* 构造函数参数 */
+              params) {
 
     // 将 arguments 对象转为数组
     var args = [].slice.call(arguments);
@@ -22,14 +25,18 @@ function Person(name, age) {
     this.age = age;
 }
 
-// 实例
-var actor = _new(Person, '张三san', 28);
-// 打印对象
-console.log(actor.valueOf())
-
-
-function Dog(name) {
-    this.name = name
+function doCreateInstanceByMock() {
+    let instance = _new(Person, "张三", 21);
+    console.log(instance.valueOf());
 }
 
-var d = new Dog("Tina");
+function doCreateInstanceByNew() {
+    let instance = new Person("张四", 22);
+    console.log(instance.valueOf());
+}
+
+doCreateInstanceByMock();
+doCreateInstanceByNew();
+
+
+
