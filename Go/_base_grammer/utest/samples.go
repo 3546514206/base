f56c34ec35d2033f8bfed420be2d37730aa0ce76
杨海波 <3546514206@qq.com>
2024-01-15 14:13:27 +0800

    创建切片的三种方式

--- Go/_base_grammer/utest/samples.go
+++ Go/_base_grammer/utest/samples.go
@@ -1,5 +1,30 @@
 package utest
 
+import "fmt"
+
 func incUpdateScore(ptrScore *int, increment int) {
 	*ptrScore += increment
+}
+
+func CreateSlice() {
+	s8 := []int{4: 5}
+	// [0 0 0 0 5]
+	fmt.Println(s8)
+	// 5 5
+	fmt.Println(len(s8), cap(s8))
+
+	const size = 5
+	const max = 10
+	s9 := make([]int, size, max)
+	// [0 0 0 0 0]
+	fmt.Println(s9)
+	// 5 10
+	fmt.Println(len(s9), cap(s9))
+
+	s10 := new([]int)
+	*s10 = append(*s10, 1, 2, 3)
+	// [1 2 3]
+	fmt.Println(*s10)
+	// 3 4
+	fmt.Println(len(*s10), cap(*s10))
 }
