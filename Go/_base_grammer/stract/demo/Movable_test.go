212b6c2c91ccd1459c30f3b2b52650277a4de144
杨海波 <3546514206@qq.com>
2024-02-20 16:43:32 +0800

    更新

--- Go/_base_grammer/stract/demo/Movable_test.go
+++ Go/_base_grammer/stract/demo/Movable_test.go
@@ -1,16 +1,17 @@
-package stract
+package demo
 
 import (
-	"_base_grammer/stract/duck"
-	"_base_grammer/stract/move"
-	"_base_grammer/stract/person"
+	"_base_grammer/stract/demo/duck"
+	"_base_grammer/stract/demo/move"
+	"_base_grammer/stract/demo/person"
+	"testing"
 )
 
 func hurryUp(movable move.Movable, distance int) int {
 	return movable.Walk(distance/2) + movable.Swim(distance/2)
 }
 
-func main() {
+func TestName(t *testing.T) {
 	d := &duck.Duck{Name: "kity", WalkSpeed: 10, SwimSpeed: 20}
 	p := &person.Person{Name: "hangman", WalkSpeed: 20, SwimSpeed: 10}
 	hurryUp(d, 2000)
