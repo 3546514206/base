a2c59b5f0a6a16aa355f9b01a00bd8677dee0495
杨海波 <3546514206@qq.com>
2024-03-04 17:17:03 +0800

    更新

---
+++ Java/_base_grammer/src/test/java/edu/zjnu/StringTest.java
@@ -0,0 +1,18 @@
+package edu.zjnu;
+
+
+import org.junit.Test;
+
+/**
+ * @author 杨海波
+ * @date 2024/3/4 16:44
+ * @description Ascii29Test
+ */
+public class StringTest {
+
+    @Test
+    public void testAscii29() {
+        System.out.println(String.valueOf((char) 29));
+    }
+}
+
