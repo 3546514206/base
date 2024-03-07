682cf92ae3c294c285b09370208266545ebb43a1
杨海波 <3546514206@qq.com>
2024-03-07 11:02:14 +0800

    test

--- Java/_base_grammer/src/test/java/edu/zjnu/StringTest.java
+++ Java/_base_grammer/src/test/java/edu/zjnu/StringTest.java
@@ -14,5 +14,18 @@ public class StringTest {
     public void testAscii29() {
         System.out.println(String.valueOf((char) 29));
     }
-}
 
+    @Test
+    public void testSubStr() {
+        String originalString = "aaaaasssss";
+
+        // 截断字符串从索引2开始到索引9（不包含索引9）
+        String startString = originalString.substring(0, originalString.length() / 2);
+        String endString = originalString.substring(originalString.length() / 2, originalString.length());
+
+
+        System.out.println("Original String: " + originalString);
+        System.out.println("startString String: " + startString);
+        System.out.println("endString String: " + endString);
+    }
+}
