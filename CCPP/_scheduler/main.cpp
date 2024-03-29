8bc434fceb25b8f5807ad897155ef436e44e6bb8
杨海波 <99322388+3546514206@users.noreply.github.com>
2024-03-29 14:57:01 +0800

    更新

--- CCPP/_scheduler/main.cpp
+++ CCPP/_scheduler/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <queue>
+#include <string>
+
+using namespace std;
+
+struct job {
+  // 任务名称
+  string name;
+  // 优先级
+  int priority;
+  // 执行时间
+  int execution_time;
+
+  job(string n, int p, int et) : name(n), priority(p), execution_time(et) {}
+
+  
+};
 
 int main() {
-    std::cout << "Hello, Cmake!" << std::endl;
-    return 0;
+
+
+  return 0;
 }
