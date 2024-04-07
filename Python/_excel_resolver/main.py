ce6fc3ab2e6cc519847edf712b2cacf5f2b8761c
杨海波 <3546514206@qq.com>
2024-04-07 12:10:23 +0800

    更新 python 目录，excel 处理

--- Python/_excel_resolver/main.py
+++ Python/_excel_resolver/main.py
@@ -2,8 +2,8 @@ from sample import handle_excel_sample
 
 
 def do_sample():
-    handle_excel_sample('./wk/分布式核心技术平台项目测试用例-技术框架和组件采购.xlsx',
-                        '分布式核心技术平台项目测试用例-技术框架和组件采购_00.xlsx')
+    handle_excel_sample('./wk/1.xlsx',
+                        '1_00.xlsx')
 
 
 # 按装订区域中的绿色按钮以运行脚本。
