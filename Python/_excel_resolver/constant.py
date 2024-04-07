1e74994a2c3f7d9dca1d4c6fae5df8c4338a5feb
杨海波 <3546514206@qq.com>
2024-04-07 12:09:20 +0800

    更新 python 目录，excel 处理

---
+++ Python/_excel_resolver/constant.py
@@ -0,0 +1,1 @@
+base_workspace_dir = "/Users/setsunayang/Documents/GitHub/base/Python/_excel_resolver/wk/"
--- Python/_excel_resolver/main.py
+++ Python/_excel_resolver/main.py
@@ -1,16 +1,11 @@
-# 这是一个示例 Python 脚本。
-
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+from sample import handle_excel_sample
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+def do_sample():
+    handle_excel_sample('./wk/分布式核心技术平台项目测试用例-技术框架和组件采购.xlsx',
+                        '分布式核心技术平台项目测试用例-技术框架和组件采购_00.xlsx')
 
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    do_sample()
