1c4f390c191b07b73d55fe1d9aec572f3937b04b
杨海波 <3546514206@qq.com>
2024-06-14 08:55:35 +0800

    更新

--- Java/_base_grammer/src/test/java/edu/zjnu/StringTest.java
+++ Java/_base_grammer/src/test/java/edu/zjnu/StringTest.java
@@ -35,4 +35,16 @@ public class StringTest {
         String secret = "#40AAAASSSSSSS";
         System.out.println(secret.substring(tagStr.length()));
     }
+
+    @Test
+    public void testString() {
+        String str = "347021a417c78d95d06dd4c4c6158368057296b55d29caff4f741385ea63922c7bb4a62bb1be8240a4e59d779a14d77c678d3ff07e5088c963f1f848c002b9d3";
+        System.out.println(str.length());
+        String key1 = str.substring(0, str.length()/2);
+        String key2 = str.substring(str.length()/2 );
+        System.out.println(key1.length());
+        System.out.println(key2.length());
+        System.out.println(key1);
+        System.out.println(key2);
+    }
 }
