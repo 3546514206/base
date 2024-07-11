fc9c9d23943af00666eaa452e7b48c3aff6afd69
杨海波 <3546514206@qq.com>
2024-07-11 13:23:10 +0800

    更新

--- TypeScript/dist/001_helloworld.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var message = "hello world";
-console.log(message);
-//# sourceMappingURL=001_helloworld.js.map
--- TypeScript/dist/002_test_class.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-class Site {
-    name() {
-        console.log("Runoob");
-    }
-}
-var obj = new Site();
-obj.name();
-//# sourceMappingURL=002_test_class.js.map
