9bc1ef7ccf8f3cf96df118e609704853827b0f26
杨海波 <3546514206@qq.com>
2024-09-04 15:44:34 +0800

    更新

---
+++ Java/_monitoring_module/micrometer_promethues/src/main/java/org/spring/springboot/MetricsConfig.java
@@ -0,0 +1,39 @@
+package org.spring.springboot;
+
+
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MetricsConfig
+ *
+ * @Date 2024-09-03 15:00
+ * @Author 杨海波
+ **/
+@Configuration
+public class MetricsConfig {
+
+    // 自定义 API 调用计数器
+    @Bean
+    public Counter customApiCallCounter(MeterRegistry meterRegistry) {
+        return Counter.builder("custom_api_calls_total")
+                .description("Counts the number of API calls to /user/login")
+                .tag("endpoint", "/user/login")  // 添加标签（标签可以帮助 Prometheus 按照维度进行聚合）
+                .register(meterRegistry);
+    }
+
+    // 自定义 API 响应时间计时器
+    @Bean
+    public Timer customApiCallTimer(MeterRegistry meterRegistry) {
+        return Timer.builder("custom_api_response_time")
+                .description("Measures the response time of /user/login")
+                .tag("endpoint", "/user/login")  // 添加标签
+                .publishPercentiles(0.5, 0.95, 0.99)  // 发布百分位（p50, p95, p99）
+                .publishPercentileHistogram(true)  // 发布百分位直方图
+                .register(meterRegistry);
+    }
+
+}
---
+++ Java/_monitoring_module/micrometer_promethues/src/main/java/org/spring/springboot/User.java
@@ -0,0 +1,39 @@
+package org.spring.springboot;
+
+/**
+ * @author: 杨海波
+ * @date: 2023-02-14 17:20:14
+ * @description: todo
+ */
+public class User {
+
+    private String id;
+
+    private String name;
+
+    private String age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
