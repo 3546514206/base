4ec65e156cf698ffee20bdb9ad9b745adb357e79
杨海波 <3546514206@qq.com>
2024-10-28 17:48:05 +0800

    字符串与字符串的占位符

---
+++ TypeScript/_base_grammer/src/others/0003.ts
@@ -0,0 +1,13 @@
+let bin_a: number = 0b0110;
+console.log(bin_a);
+
+let octal_b: number = 0o17;
+console.log(octal_b);
+
+let dec_c: number = 6;
+console.log(dec_c);
+
+let str_name: string = "setsuna";
+let year: number = 9;
+let words: string = `this is ${str_name}'s ${year + 2} year`;
+console.log(words);
