8848e25f1e547211d2b3d2d0e5d272b4cd10f3e2
杨海波 <3546514206@qq.com>
2024-10-30 14:50:52 +0800

    Number 的一些常见用法

---
+++ TypeScript/_base_grammer/src/others/0005.ts
@@ -0,0 +1,9 @@
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.NaN);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+
+let aab = 1829.181902
+// 转换为科学计数法
+console.log(aab.toExponential());
