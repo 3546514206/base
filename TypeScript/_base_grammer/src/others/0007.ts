17610da1c12d53002b02cf994be1b67a16ed85cf
杨海波 <3546514206@qq.com>
2024-10-30 16:33:30 +0800

    接口类型案例

---
+++ TypeScript/_base_grammer/src/others/0007.ts
@@ -0,0 +1,23 @@
+interface IPerson {
+    name: string;
+
+    age: number;
+
+    // () => void：这是该方法的类型定义，包含以下几个部分：
+    // ()：表示这个方法没有参数。如果方法有参数，它们会被列在这里。
+    // =>：箭头符号，用于表示方法的返回类型。
+    // void：表示方法没有返回值，即调用该方法时不会返回任何数据。
+    say: () => void;
+}
+
+let employ: IPerson = {
+    name: "setsuna",
+
+    age: 21,
+
+    say: () => {
+        console.log(`my name is ${employ.name},my age is ${employ.age}`);
+    }
+}
+
+employ.say();
