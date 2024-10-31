fda0575cbc9a86900f263d686c95fa4906b9e37a
杨海波 <3546514206@qq.com>
2024-10-31 16:00:23 +0800

    typescript实现继承

---
+++ TypeScript/_base_grammer/src/others/0010.ts
@@ -0,0 +1,24 @@
+interface IPersonV2 {
+    name: string;
+    age: number;
+}
+
+class Chef implements IPersonV2 {
+
+    age: number;
+    name: string;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    cook(): void {
+        console.log(this.name);
+        console.log(this.age);
+        console.log("cooking!!!");
+    }
+}
+
+let pe: Chef = new Chef("test", 21);
+pe.cook();
