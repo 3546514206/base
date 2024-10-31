aa6735163932d6415ae7eedaa9d6fc86815f2600
杨海波 <3546514206@qq.com>
2024-10-31 17:07:49 +0800

    命名空间

---
+++ TypeScript/_base_grammer/src/others/012、接口的隐式实现.ts
@@ -0,0 +1,28 @@
+namespace implicit_implementation {
+    interface Person {
+        name: string;
+        age: number;
+
+        greet(): void;
+    }
+
+    // 隐式实现接口（没有使用 `implements` 关键字）
+    class Developer {
+        private readonly name: string;
+        private readonly age: number;
+
+        constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+        }
+
+        greet() {
+            console.log(`Hello, I'm ${this.name}`);
+        }
+    }
+
+    // 成功，符合 Person 的类型
+    const dev: Person = new Developer("Alice", 30);
+
+    dev.greet();
+}
