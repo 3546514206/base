809ad6dbf921afe214f296ed7b27b39fa0bcdfd4
杨海波 <3546514206@qq.com>
2025-04-05 13:25:46 +0800

    更新

---
+++ Vue/vue-demo/vue-demo-001/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld str="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {HelloWorld},
+  data() {
+  }
+};
+</script>
+
+<style>
+</style>
