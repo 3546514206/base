c8c98fedec340c1c1f490d69d9d23a5568804bdc
杨海波 <3546514206@qq.com>
2025-09-28 16:32:57 +0800

    map 测试

---
+++ Java/_base_grammer/src/main/java/MapMain.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * MapMain
+ *
+ * @Date 2025-09-22 09:07
+ * @Author 杨海波
+ **/
+public class MapMain {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        System.out.println(map.get(null));
+    }
+}
