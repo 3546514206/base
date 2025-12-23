1d996c2871daad8e47667da58f7fb0d5c8027ffd
杨海波 <3546514206@qq.com>
2025-12-23 10:28:21 +0800

    更新

---
+++ Vue/simply-calculator-vuejs-master/README.md
@@ -0,0 +1,35 @@
+# vue-webpack-calculator
+
+> 用vue.js实现简易计算器<br>
+>A simply vue-calculator built by vue2.0 + vue-cli (webpack- simple) ,<br> fast way to get through vue.js , vue-loader and webpack .
+<br>
+
+Grade that this demo has been merged into the project :<a href="https://github.com/vuejs/awesome-vue">awesome-vue</a>:yum:<br>
+And here is another vue demo <a href="https://github.com/CaiYiLiang/vue-demos/tree/master/wikipediaViewer-vuejs">wikipediaViewer-vuejs</a> ,which is also merged into the project :<a href="https://github.com/vuejs/awesome-vue">awesome-vue</a>:yum:
+
+
+# demo
+<img src="./calculator_vuejs.gif" alt="calculator.vuejs-demo" width="360px" height="auto">
+<br>
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+<br>
+
+## todo item
+- add keycode eventListener
+<br>
+
+ヾ(o◕∀◕)ﾉヾ 如果有那么一丁点儿喜欢 请随手🌟~(≧▽≦)/~啦啦啦 <br>
+ヾ(o◕∀◕)ﾉヾ Encourage me a start🌟 if you like itヾ(o◕∀◕)ﾉヾ 
---
+++ Vue/simply-calculator-vuejs-master/src/Calculator.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="calculator">
+    <header>
+      <h1>Calculator</h1>
+      <span class="author">BY <a href="https://www.freecodecamp.com/caiyiliang" target="_blank">CaiYiLiang</a></span>
+    </header>
+    <section class="calculator" id="calculator">
+      <calculator-content></calculator-content>
+    </section>
+  </div>
+</template>
+
+<script>
+import CalculatorContent from 'components/CalculatorContent.vue'
+
+export default {
+  name: 'calculator',
+  components: {
+    CalculatorContent
+  }
+}
+
+</script>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Pacifico');
+
+
+html {
+  font-size: 62.5%; /* =10px */
+}
+
+body {
+  background-color: #f4f1f1; /* HEX */
+}
+
+
+header {
+  font-size: 1.8rem;
+  font-family: 'Pacifico', cursive;
+}
+
+header .author {
+  font-size: 1.5rem;
+}
+
+header .author a {
+  text-decoration: none;
+}
+
+header, .calculator {
+  margin: 2.5rem auto;
+  text-align: center;
+}
+
+.calculator {
+  border: 0.8rem solid #f67373;
+  width: 34rem;
+  height: 40rem;
+  background-color: #f28080;
+  font-size: 1.6rem;
+  -webkit-border-radius: 1rem;
+  -moz-border-radius: 1rem;
+  -ms-border-radius: 1rem;
+  -o-border-radius: 1rem;
+  border-radius: 1rem;: -webkit-box-shadow: 1 rem 1 rem 0.5 rem #ccc;
+  box-shadow: 1rem 1rem 0.5rem #ccc;
+
+}
+
+
+</style>
