b2ad9779ecb89a5726798df0d75485ca2d37dfb4
杨海波 <3546514206@qq.com>
2026-03-12 01:02:47 +0800

    更行

--- Java/_java_guide/src/main/java/edu/zjnu/c10/CompFiles.java
+++ Java/_java_guide/src/main/java/edu/zjnu/c10/CompFiles.java
@@ -1,4 +1,10 @@
 package edu.zjnu.c10;
 
+import java.util.UUID;
+
 public class CompFiles {
+
+    public static void main(String[] args) {
+        System.out.println(UUID.randomUUID());
+    }
 }
